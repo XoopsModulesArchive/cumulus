1a1a8615be91bf133bd3dbfc6224f40509c7ec66
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-12-11 17:04:52 +0000

    Adding Cumulus module

---
+++ cumulus/language/english/blocks.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Tag management for XOOPS
+ *
+ * @copyright	The XOOPS project http://www.xoops.org/
+ * @license		http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @author		Susheng Yang (ezsky or ezskycn or skorpio) <ezskyyoung@hotmail.com> 
+ * @since		1.00
+ * @version		$Id: blocks.php 
+ * @package		cumulus
+ */
+
+if (!defined('XOOPS_ROOT_PATH')) { exit(); }
+
+define("TAG_MB_ITEMS", "Number to display");
+define("TAG_MB_FONTSIZE_MAX", "Maximum font-size(%) for display");
+define("TAG_MB_FONTSIZE_MIN", "Minimum font-size(%) for display");
+define("TAG_MB_TIME_DURATION", "Time duration for stats, in days");
+define("TAG_MB_FLASH_TRANSPARENCY", "flash transparency");
+define("TAG_MB_FLASH_WIDTH", "flash width");
+define("TAG_MB_FLASH_HEIGHT", "flash height");
+define("TAG_MB_FLASH_BACKGROUND", "flash background");
+define("TAG_MB_FLASH_TRANSPARENT","transparent");
+define("TAG_MB_FLASH_MINFONTCOLOR","min font color");
+define("TAG_MB_FLASH_MAXFONTCOLOR","max font color");
+define("TAG_MB_FLASH_HILIGHTFONTCOLOR","font hilight color");
+define("TAG_MB_FLASH_SPEED","flash speed");
+?>
