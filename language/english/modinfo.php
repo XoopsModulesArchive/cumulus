c5a97852083bc5d2e2e73aff7737b7fa285c5e34
mambax7 <mambax7@gmail.com>
2016-01-18 05:51:34 -0500

    Bring everything back up one level

---
+++ language/english/modinfo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * cumulus tag cloud for XOOPS
+ *
+ * @copyright	The XOOPS project http://www.xoops.org/
+ * @license		http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @author		Susheng yang (ezsky or skorpio) <ezskyyoung@hotmail.com>
+ * @since		1.00
+ * @version		$Id: modinfo.php 
+ * @package		cumulus
+ */
+
+if (!defined('XOOPS_ROOT_PATH')) { exit(); }
+
+define("_MI_CUMULUS_NAME","cumulus");
+define("_MI_CUMULUS_DESC","cumulus in xoops");
+
+define("_MI_BLOCK_CUMULUS","Cumulus Tag Cloud");
+define("_MI_BLOCK_CUMULUS_DESC","");
+
+?>
